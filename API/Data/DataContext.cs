9a98452a30e95376af7bd8a6e8adbc20ca5e6e67
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-04-23 09:59:54 +0200

    DBContext addition

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,7 +1,6 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 //using System.Threading.Tasks;
 
 
@@ -9,6 +8,11 @@ namespace API.Data
 {
     public class DataContext: DbContext
     {
-        
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<AppUser>Users { get; set; }
+
     }
 }

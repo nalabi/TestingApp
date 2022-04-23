198754424d9a4ac653207d8987a41393ca7f4c09
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-04-23 08:48:01 +0200

    Saturday commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int id { get; set; }
+        public string UserName  { get; set; }
+
+       
+        
+    }
+}

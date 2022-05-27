dba7183dcb9dd1f0a5dbf3e15ac71617ff9d6cc5
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-05-27 06:43:47 +0200

    test view

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,19 +6,22 @@ using System.Security.Cryptography;
 using System.Text;
 using API.DTOs;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
 
 namespace API.Controllers
 {
     public class AccountController:BaseApiController
     {
+        private readonly ITokenService _tokenService;
         public readonly DataContext _context;
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService )
         {
+           _tokenService = tokenService;
            _context = context;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
@@ -32,10 +35,13 @@ namespace API.Controllers
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
             var user = await _context.Users
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
@@ -48,7 +54,10 @@ namespace API.Controllers
             for (int i =0; i < computedHash.Length; i++){
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username){

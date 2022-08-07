576a68ba3523d8ffbadbd62c00dd1315ca803906
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-08-07 12:59:25 +0200

    145

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {

5f312fc0f3ad57e3eda40d733c87b124f0378724
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-08-07 13:30:24 +0200

    658

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Entities;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace API.Data
 {
@@ -16,6 +17,18 @@ namespace API.Data
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users= JsonSerializer.Deserialize<List<AppUser>>(userData);
+            foreach(var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower(), 
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("TestApp"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
         }
     }
 }

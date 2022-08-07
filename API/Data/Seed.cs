a960210b2067332b9937e4a926a0a6d63c41c8e7
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-08-07 12:33:33 +0200

    77887

---
+++ API/Data/Seed.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users= JsonSerializer.Deserialize<List<AppUser>>(userData);
+        }
+    }
+}

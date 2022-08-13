e460aa124eb72f1648aba7bad81b7e2461e8630e
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-08-13 23:34:09 +0200

    45

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,48 +1,49 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-baseUrl='https://localhost:5001/api/';
-private currentUserSource = new ReplaySubject<User>(1);
-currentUser$ = this.currentUserSource.asObservable();
-  constructor(private http:HttpClient) { }
-  login(model: any){
-    return this.http.post(this.baseUrl +'account/login', model ).pipe(
-      map((response:User)=>{
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next (user);
-
+          this.currentUserSource.next(user);
         }
-      }
-      )
-
+      })
     )
   }
-  register(model: any){
+
+  register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
-        if (user)
-        {
-          localStorage.setItem('user', JSON.stringify(user) );
-          this.currentUserSource.next();
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
-        return user;
       })
-    );
-  
-    }
-  setCurrentUser(user:User){
+    )
+  }
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
-  logout(){
+
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }

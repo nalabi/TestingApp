9509652b5e9a8ce05d2d3af5b43636c8a7f604ce
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-08-14 14:15:01 +0200

    6987

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -18,7 +19,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children:[
       {path:'members', component: MemberListComponent, canActivate:[AuthGuard] },
-      {path:'members/:id', component: MemberDetailComponent},
+      {path:'members/:username', component: MemberDetailComponent},
       {path:'lists', component: ListsComponent},
       {path:'messages', component: MessagesComponent},
     ]

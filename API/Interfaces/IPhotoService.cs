36e73328ff629dc4af116998525af41af4587c5b
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-09-16 17:43:04 +0200

    4528

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,13 @@
+using System;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(String publicId);
+        
+    }
+}

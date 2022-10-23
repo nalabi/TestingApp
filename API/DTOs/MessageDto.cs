2291b2bba6f1a36346d064a96c1d8170d44b2ed8
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-10-23 12:47:53 +0200

    Unit Of Work

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace API.DTOs
 {
@@ -22,8 +23,12 @@ namespace API.DTOs
 
         public DateTime? DateRead { get; set; }
 
-        public DateTime? MessageSent { get; set; } 
+        public DateTime? MessageSent { get; set; }
 
- 
+        [JsonIgnore]
+        public bool SenderDeleted { get; set; }
+
+        [JsonIgnore]
+        public bool RecipientDeleted { get; set; }
     }
 }
--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 //import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
@@ -9,13 +10,13 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
 
-  //constructor(private confirmService: ConfirmService) {}
+  constructor(private confirmService: ConfirmService) {}
 
   //canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
-    canDeactivate(component: MemberEditComponent): boolean {
+    canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
     if (component.editForm.dirty) {
-     //return this.confirmService.confirm()
-    return confirm('Are you sure you want to continue? Any unsaved changes will ne lost');
+    
+    return this.confirmService.confirm()
     }
     return true;
   }

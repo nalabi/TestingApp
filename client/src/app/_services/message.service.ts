f603dfba1030f2c65e11c70dc8dc9a8938bb445e
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-11-02 18:51:30 +0200

    Test

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HubConnectionBuilder } from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
@@ -15,13 +16,14 @@ import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 export class MessageService {
   baseUrl = environment.apiUrl;
   hubUrl = environment.hubUrl;
-  private hubConnection;
-  private messageThreadSource = new BehaviorSubject<Message[]>([])
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private busyService: BusyService) { }
 
   createHubConnection(user: User, otherUsername: string) {
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
         accessTokenFactory: () => user.token
@@ -29,27 +31,28 @@ export class MessageService {
       .withAutomaticReconnect()
       .build()
 
-    this.hubConnection.start().catch(error => console.log(error));
+    this.hubConnection.start()
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
     })
+
     this.hubConnection.on('NewMessage', message => {
-      this.messageThreadSource.pipe(take(1)).subscribe(messages => {
+      this.messageThread$.pipe(take(1)).subscribe(messages => {
         this.messageThreadSource.next([...messages, message])
-      }
-      )
+      })
     })
 
-    this.hubConnection.on('UpdatedGroup', (group: Group) =>{
-      if(group.connections.some(x => x.username === otherUsername )){
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some(x => x.username === otherUsername)) {
         this.messageThread$.pipe(take(1)).subscribe(messages => {
           messages.forEach(message => {
-            if (!message.dateRead){
+            if (!message.dateRead) {
               message.dateRead = new Date(Date.now())
             }
           })
-
           this.messageThreadSource.next([...messages]);
         })
       }
@@ -58,9 +61,9 @@ export class MessageService {
 
   stopHubConnection() {
     if (this.hubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
-
   }
 
   getMessages(pageNumber, pageSize, container) {
@@ -74,11 +77,11 @@ export class MessageService {
   }
 
   async sendMessage(username: string, content: string) {
-    return this.hubConnection.invoke('SendMessage', { recipientUsername: username, content })
+    return this.hubConnection.invoke('SendMessage', {recipientUsername: username, content})
       .catch(error => console.log(error));
   }
 
   deleteMessage(id: number) {
-    return this.http.delete(this.baseUrl + 'messages/' + id)
+    return this.http.delete(this.baseUrl + 'messages/' + id);
   }
 }

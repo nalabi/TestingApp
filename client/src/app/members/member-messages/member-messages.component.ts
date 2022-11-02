063a0fb13a168d21a0ef4057b9e3f0aa5a2511d0
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-11-02 17:29:17 +0200

    123

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
+import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
@@ -10,21 +11,22 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-@ViewChild('messageForm') messageForm: NgForm;
-@Input() messages: Message[];
-@Input() username: string;
-messageContent: string;
+  @ViewChild('messageForm') messageForm: NgForm;
+  @Input() messages: Message[];
+  @Input() username: string;
+  messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
-    
   }
-sendMessage(){
-  this.messageService.sendMessage(this.username, this.messageContent).then(() => message =>
-    {
-       this.messageForm.reset();
-    })
-}
+
+  sendMessage() {
+    this.loading = true;
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm.reset();
+    }).finally(() => this.loading = false);
+  }
 
 }

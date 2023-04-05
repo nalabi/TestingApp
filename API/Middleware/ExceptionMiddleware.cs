9026048314cc7f6823c49bd01edd2309b496b48f
Bongumusa Nala <bongimusa.nala@gmail.com>
2023-04-05 19:47:08 +0200

    11

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -26,7 +26,7 @@ namespace API.Middleware
         {
             try
             {
-               // await _next(context);
+               await _next(context);
             }
             catch (Exception ex)
             {
